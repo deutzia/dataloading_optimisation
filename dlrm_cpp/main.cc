8dee11093d9a3af162bb6f26181e6eb5c81fc214
Iwona Kotlarska <pinus314@gmail.com>
2020-03-15 17:52:26 +0100

    Add autoformattig for cpp code

--- dlrm_cpp/main.cc
+++ dlrm_cpp/main.cc
@@ -1,12 +1,12 @@
-#include<csv.h>
-#include<cnpy.h>
-#include<complex>
-#include<cstdlib>
-#include<iostream>
-#include<map>
-#include<string>
-#include<getopt.h>
-#include<unordered_map>
+#include <cnpy.h>
+#include <complex>
+#include <cstdlib>
+#include <csv.h>
+#include <getopt.h>
+#include <iostream>
+#include <map>
+#include <string>
+#include <unordered_map>
 
 constexpr uint32_t NUM_INT = 13;
 constexpr uint32_t NUM_CAT = 26;
@@ -37,7 +37,8 @@ uint32_t parse_cat(const char *data)
     uint32_t result = 0;
     for (int i = 0; i < 8; i++)
     {
-        unsigned int data_value = (data[i] - ((data[i] >= '0' && data[i] <= '9') ? '0' : ('a' - 10)));
+        unsigned int data_value =
+            (data[i] - ((data[i] >= '0' && data[i] <= '9') ? '0' : ('a' - 10)));
         result += data_value << (28 - 4 * i);
     }
 
@@ -54,22 +55,20 @@ int main(int argc, char *argv[])
     while (1)
     {
         int c;
-        static struct option long_options[] =
-        {
+        static struct option long_options[] = {
             /* These options don’t set a flag.
                  We distinguish them by their indices. */
-            {"max-ind-range",           optional_argument, 0, 'a'},
-            {"data-sub-sample-rate",    optional_argument, 0, 'b'},
-            {"data-randomize",          optional_argument, 0, 'c'},
-            {"memory-map",              optional_argument, 0, 'd'},
-            {"raw-data-file",           optional_argument, 0, 'e'},
-            {"processed-data-file",     optional_argument, 0, 'f'},
-            {0, 0, 0, 0}
-        };
+            {"max-ind-range", optional_argument, 0, 'a'},
+            {"data-sub-sample-rate", optional_argument, 0, 'b'},
+            {"data-randomize", optional_argument, 0, 'c'},
+            {"memory-map", optional_argument, 0, 'd'},
+            {"raw-data-file", optional_argument, 0, 'e'},
+            {"processed-data-file", optional_argument, 0, 'f'},
+            {0, 0, 0, 0}};
         /* getopt_long stores the option index here. */
         int option_index = 0;
 
-        c = getopt_long (argc, argv, "", long_options, &option_index);
+        c = getopt_long(argc, argv, "", long_options, &option_index);
 
         /* Detect the end of the options. */
         if (c == -1)
@@ -77,47 +76,51 @@ int main(int argc, char *argv[])
 
         switch (c)
         {
-            case 0:
-                printf ("option %s", long_options[option_index].name);
-                if (optarg)
-                    printf (" with arg %s", optarg);
-                printf ("\n");
-                break;
+        case 0:
+            printf("option %s", long_options[option_index].name);
+            if (optarg)
+                printf(" with arg %s", optarg);
+            printf("\n");
+            break;
 
-            case 'a':
-                max_ind_range = std::stoi(optarg);
-                break;
+        case 'a':
+            max_ind_range = std::stoi(optarg);
+            break;
 
-            case 'b':
-                data_sub_sample_rate = std::stof(optarg);
-                break;
+        case 'b':
+            data_sub_sample_rate = std::stof(optarg);
+            break;
 
-            case 'c':
-                data_randomize = optarg;
-                break;
+        case 'c':
+            data_randomize = optarg;
+            break;
 
-            case 'd':
-                memory_map = true;
-                break;
+        case 'd':
+            memory_map = true;
+            break;
 
-            case 'e':
-                raw_data_file = optarg;
-                break;
+        case 'e':
+            raw_data_file = optarg;
+            break;
 
-            case 'f':
-                processed_data_file = optarg;
-                break;
+        case 'f':
+            processed_data_file = optarg;
+            break;
 
-            case '?':
-                /* getopt_long already printed an error message. */
-                break;
+        case '?':
+            /* getopt_long already printed an error message. */
+            break;
 
-            default:
-                abort();
+        default:
+            abort();
         }
     }
 
-//    printf("max_ind_range=%d data_sub_sample_rate=%f memory_map=%d data_randomize=%s data_set=%s raw_data_file=%s processed_data_file=%s\n", max_ind_range, data_sub_sample_rate, memory_map, data_randomize.c_str(), data_set.c_str(), raw_data_file.c_str(), processed_data_file.c_str());
+    //    printf("max_ind_range=%d data_sub_sample_rate=%f memory_map=%d
+    //    data_randomize=%s data_set=%s raw_data_file=%s
+    //    processed_data_file=%s\n", max_ind_range, data_sub_sample_rate,
+    //    memory_map, data_randomize.c_str(), data_set.c_str(),
+    //    raw_data_file.c_str(), processed_data_file.c_str());
 
     /* Print any remaining command line arguments (not options). */
     if (optind < argc)
@@ -127,7 +130,8 @@ int main(int argc, char *argv[])
     }
 
     std::cout << "Opening: " << raw_data_file << "\n";
-    io::CSVReader<40, io::trim_chars<>, io::no_quote_escape<'\t'>> in(raw_data_file);
+    io::CSVReader<40, io::trim_chars<>, io::no_quote_escape<'\t'>> in(
+        raw_data_file);
 
     int type;
     char *x_int[NUM_INT];
@@ -139,25 +143,22 @@ int main(int argc, char *argv[])
     for (uint32_t i = 0; i < NUM_CAT; ++i)
         counter[i] = 0;
     while (in.read_row(
-            //type
-            type,
-            //13 features taking integer values
-            x_int[0], x_int[1], x_int[2], x_int[3], x_int[4],
-            x_int[5], x_int[6], x_int[7], x_int[8], x_int[9],
-            x_int[10], x_int[11], x_int[12],
-            //26 categorical features
-            x_cat[0], x_cat[1], x_cat[2], x_cat[3], x_cat[4],
-            x_cat[5], x_cat[6], x_cat[7], x_cat[8], x_cat[9],
-            x_cat[10], x_cat[11], x_cat[12], x_cat[13], x_cat[14],
-            x_cat[15], x_cat[16], x_cat[17], x_cat[18], x_cat[19],
-            x_cat[20], x_cat[21], x_cat[22], x_cat[23], x_cat[24],
-            x_cat[25]
-    ))
+        // type
+        type,
+        // 13 features taking integer values
+        x_int[0], x_int[1], x_int[2], x_int[3], x_int[4], x_int[5], x_int[6],
+        x_int[7], x_int[8], x_int[9], x_int[10], x_int[11], x_int[12],
+        // 26 categorical features
+        x_cat[0], x_cat[1], x_cat[2], x_cat[3], x_cat[4], x_cat[5], x_cat[6],
+        x_cat[7], x_cat[8], x_cat[9], x_cat[10], x_cat[11], x_cat[12],
+        x_cat[13], x_cat[14], x_cat[15], x_cat[16], x_cat[17], x_cat[18],
+        x_cat[19], x_cat[20], x_cat[21], x_cat[22], x_cat[23], x_cat[24],
+        x_cat[25]))
     {
         for (uint32_t q = 0; q < NUM_INT; q++)
         {
             parse_int(x_int[q]);
-            //TODO
+            // TODO
         }
         for (uint32_t q = 0; q < NUM_CAT; q++)
         {
@@ -171,7 +172,7 @@ int main(int argc, char *argv[])
             {
                 new_values[q] = convert_dicts[q][num];
             }
-            //TODO
+            // TODO
         }
     }
 }
